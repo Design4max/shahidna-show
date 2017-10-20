1baa5cb09fea4db1bce2d52466152a47ada765df
Design4max <haider991amara.hk@gmail.com>
2017-10-20 23:27:34 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,5 @@
 /*** Defined variables 
  *********************/
-
 var player          = document.querySelector("#player"),
     vid             = document.querySelector("#video"),
     playPauseBtn    = document.querySelector(".playpause-btn"),
@@ -15,11 +14,8 @@ var player          = document.querySelector("#player"),
     volumeBar       = document.querySelector(".volume-bar"),
     progressVol     = document.querySelector(".progress-vol"),
     buffer          = document.querySelector(".buffer"),curTime,seekTime;
-
-
 /*** Event listeners
  ********************/
-
 playPauseBtn.addEventListener("click", playPause, false);
 vid.addEventListener("click", playPause, false);
 vid.addEventListener("playing", duration, false);
@@ -33,22 +29,17 @@ vid.addEventListener("timeupdate", seekUpdate, false);
 volumeBtn.addEventListener("click", mutedVol, false);
 volumeBar.addEventListener("click", seekVol, false);
 volumeBar.addEventListener("click", volUpdate, false);
-
-
-/*** Current Time & Duration
+/*** Current Time &amp; Duration
  ****************************/
-
-
 function playPause(){
     if(vid.paused){
-        playPauseBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+        playPauseBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true" />';
         vid.play();
     }else{
-        playPauseBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+        playPauseBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true" />';
         vid.pause();
     }
 }
-
 function duration(){
     
     var secDuration     = Math.floor(vid.duration %60 %60),
@@ -60,165 +51,144 @@ function duration(){
     
     if(hourDuration != 0){
         
-        if(hourDuration < 10){
+        if(hourDuration &lt; 10){
             dur.innerHTML += "0" + hourDuration;
         }
-        if(hourDuration < 60 & hourDuration > 9){
+        if(hourDuration &lt; 60 &amp; hourDuration &gt; 9){
             dur.innerHTML += hourDuration;
         }
         
-        if(minDuration < 10){
+        if(minDuration &lt; 10){
             dur.innerHTML += ":0" + minDuration;
         }
-        if(minDuration < 60 & minDuration > 9){
+        if(minDuration &lt; 60 &amp; minDuration &gt; 9){
            dur.innerHTML += ":" + minDuration;
         }
-
     }else{
         
-        if(minDuration < 10){
+        if(minDuration &lt; 10){
             dur.innerHTML += "0" + minDuration;
         }
-        if(minDuration < 60 & minDuration > 9){
+        if(minDuration &lt; 60 &amp; minDuration &gt; 9){
            dur.innerHTML += minDuration;
         }
         
     }
     
-    if(secDuration < 10){
+    if(secDuration &lt; 10){
        dur.innerHTML += ":0" + secDuration;
     }
-    if(secDuration < 60 & secDuration > 9){
+    if(secDuration &lt; 60 &amp; secDuration &gt; 9){
        dur.innerHTML += ":" + secDuration;
     }
     
     
 }
-
 function current(){
     
-    var curTime         = vid.currentTime,
-        secCurTime      = Math.floor(curTime %60),
-        minCurTime      = Math.floor(curTime /60) %60,
-        hourCurTime     = Math.floor(curTime /60 /60);
+    curTime         = vid.currentTime;
+    secCurTime      = Math.floor(curTime %60);
+    minCurTime      = Math.floor(curTime /60) %60;
+    hourCurTime     = Math.floor(curTime /60 /60);
     
     cur.innerHTML = "";
     
     if(hourCurTime != 0){
         
-        if(hourCurTime < 10){
+        if(hourCurTime &lt; 10){
             cur.innerHTML += "0" + hourCurTime;
         }
-        if(hourCurTime < 60 & hourCurTime > 9){
+        if(hourCurTime &lt; 60 &amp; hourCurTime &gt; 9){
             cur.innerHTML += hourCurTime;
         }
-
-        if(minCurTime < 10){
+        if(minCurTime &lt; 10){
             cur.innerHTML += ":0" + minCurTime;
         }
-        if(minCurTime < 60 & minCurTime > 9){
+        if(minCurTime &lt; 60 &amp; minCurTime &gt; 9){
            cur.innerHTML += ":" + minCurTime;
         }
        
     }else{
-
-        if(minCurTime < 10){
+        if(minCurTime &lt; 10){
             cur.innerHTML += "0" + minCurTime;
         }
-        if(minCurTime < 60 & minCurTime > 9){
+        if(minCurTime &lt; 60 &amp; minCurTime &gt; 9){
            cur.innerHTML += minCurTime;
         }
     }
     
     
-    if(secCurTime < 10){
+    if(secCurTime &lt; 10){
        cur.innerHTML += ":0" + secCurTime;
     }
-    if(secCurTime < 60 & secCurTime > 9){
+    if(secCurTime &lt; 60 &amp; secCurTime &gt; 9){
        cur.innerHTML += ":" + secCurTime;
     }
     
       
     
 }
-
 function progBuffer(){
-    if (vid.duration > 0) {
+    if (vid.duration &gt; 0) {
         buffer.style.width = ((vid.buffered.end(vid.buffered.length - 1) / vid.duration)*100) + "%";
     }
 }
-
 function SeekBuffer(){
     
     var inc = progressBar.width / vid.duration;
         
-    for (i = 0; i < vid.buffered.length; i++) {
-
+    for (i = 0; i &lt; vid.buffered.length; i++) {
         var startX = vid.buffered.start(i) * inc;
         var endX = vid.buffered.end(i) * inc;
         var width = endX - startX;
-
         buffer.style.width = width + "%";
     }
 }
-
 function seekAble(e){
-    var seekTime = vid.duration * ((e.pageX - progressBar.offsetLeft) / progressBar.offsetWidth);
+    seekTime = vid.duration * ((e.pageX - progressBar.offsetLeft) / progressBar.offsetWidth);
     vid.currentTime = seekTime;
 }
-
 function seekUpdate(){
     
-    if (vid.duration > 0) {
+    if (vid.duration &gt; 0) {
       progress.style.width = ((vid.currentTime / vid.duration)*100) + "%";
     }
 }
-
 function seekTimeTxt(e){
     
-    var seccurSeek      = Math.floor(vid.duration * (e.pageX - progressBar.offsetLeft ) / progressBar.offsetWidth) %60,
-        mincurSeek      = Math.floor((vid.duration * (e.pageX - progressBar.offsetLeft) / progressBar.offsetWidth) /60) %60,
-        hourcurSeek     = Math.floor((vid.duration * (e.pageX  - progressBar.offsetLeft) / progressBar.offsetWidth) /60 /60),
-        visibilityError = Math.sign(vid.duration * (e.pageX - progressBar.offsetLeft ) / progressBar.offsetWidth);
-
+    seccurSeek      = Math.floor(vid.duration * (e.pageX - progressBar.offsetLeft ) / progressBar.offsetWidth) %60;
+    mincurSeek      = Math.floor((vid.duration * (e.pageX - progressBar.offsetLeft) / progressBar.offsetWidth) /60) %60;
+    hourcurSeek     = Math.floor((vid.duration * (e.pageX  - progressBar.offsetLeft) / progressBar.offsetWidth) /60 /60);
+    visibilityError = Math.sign(vid.duration * (e.pageX - progressBar.offsetLeft ) / progressBar.offsetWidth);
     curSeek.innerHTML = "";
-
     if(hourcurSeek != 0){
-
-        if(hourcurSeek < 10){
+        if(hourcurSeek &lt; 10){
             curSeek.innerHTML += "0" + hourcurSeek;
         }
-        if(hourcurSeek < 60 & hourcurSeek > 9){
+        if(hourcurSeek &lt; 60 &amp; hourcurSeek &gt; 9){
             curSeek.innerHTML += hourcurSeek;
         }
-
-        if(mincurSeek < 10){
+        if(mincurSeek &lt; 10){
             curSeek.innerHTML += ":0" + mincurSeek;
         }
-        if(mincurSeek < 60 & mincurSeek > 9){
+        if(mincurSeek &lt; 60 &amp; mincurSeek &gt; 9){
            curSeek.innerHTML += ":" + mincurSeek;
         }
-
     }
     else{
-
-        if(mincurSeek < 10){
+        if(mincurSeek &lt; 10){
             curSeek.innerHTML += "0" + mincurSeek;
         }
-        if(mincurSeek < 60 & mincurSeek > 9){
+        if(mincurSeek &lt; 60 &amp; mincurSeek &gt; 9){
            curSeek.innerHTML += mincurSeek;
         }
-
     }
-
-
-    if(seccurSeek < 10){
+    if(seccurSeek &lt; 10){
         curSeek.innerHTML += ":0" + seccurSeek;
     }
-    if(seccurSeek < 60 & seccurSeek > 9){
+    if(seccurSeek &lt; 60 &amp; seccurSeek &gt; 9){
         curSeek.innerHTML += ":" + seccurSeek;
     }
-
     progressBar.addEventListener("mousemove", function(){
         if(visibilityError == -1){
             curSeek.innerHTML = "00:00";
@@ -232,28 +202,24 @@ function seekTimeTxt(e){
     
     
 }
-
 function seekTimeTxtHide(){
     curSeek.style.visibility    = "hidden";
 }
-
 function mutedVol(){
     
     if(vid.volume != 0){
-        volumeBtn.innerHTML = '<i class="fa fa-volume-off" aria-hidden="true"></i>';
+        volumeBtn.innerHTML = '<i class="fa fa-volume-off" aria-hidden="true" />';
         vid.volume = 0;
     }else{
-        volumeBtn.innerHTML = '<i class="fa fa-volume-up" aria-hidden="true"></i>';
+        volumeBtn.innerHTML = '<i class="fa fa-volume-up" aria-hidden="true" />';
         vid.volume = 1;
     }
     
 }
-
 function seekVol(e){
     seekVol = (e.pageX - volumeBar.offsetLeft) / volumeBar.offsetWidth;
     vid.volume = seekVol;
 }
-
 function volUpdate(){
     progressVol.style.width = vid.volume * 100 + "%";
 }
